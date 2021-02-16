430a6032690bc2c2d489ca40bb008d7d41d794a7
Pavel Seleznev <yalutorovsk4@gmail.com>
2021-02-17 00:43:56 +0300

    Create vui python package

---
+++ vui/vui/config.py
@@ -0,0 +1,18 @@
+build = 1
+
+# Paths
+experiments_dir = "D:\\Projects\\RoboticVUI\\vui\\vui\\experiments\\"
+ref_words_dir = "D:\\Projects\\RoboticVUI\\vui\\vui\\resources\\ref_words\\"
+datasets_dir = "C:\\Datasets\\Audio\\"
+checkpoint_prefix = "checkpoint-"
+
+# Recognition
+min_word_weight = 0.75
+silence_word = "_silence"
+unknown_word = "_unknown"
+
+# Training
+training_steps = 100
+display_interval = 100
+checkpoint_interval = 100
+verbose = False

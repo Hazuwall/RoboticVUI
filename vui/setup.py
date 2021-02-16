f4b075dfb1e7fba6415dde687b7265ca13bae111
Pavel Seleznev <yalutorovsk4@gmail.com>
2021-02-17 02:22:17 +0300

    Add vui node

--- vui/setup.py
+++ vui/setup.py
@@ -19,4 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=["matplotlib", "numpy", "tensorflow>=2.0.0", "coolname", "librosa", "h5py", "soundfile", "termcolor"]
 )

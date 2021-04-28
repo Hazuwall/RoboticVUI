5bbbdaacc36ce7f822a33d1825cfc555d9cec39f
Pavel Seleznev <yalutorovsk4@gmail.com>
2021-04-28 22:42:02 +0300

    Add experiment 5: 2 LSTM

---
+++ vui/vui/experiments/5_f1_2lstm_encoder_60_aug/frontend.py
@@ -0,0 +1,20 @@
+import numpy as np
+import vui.frontend.dsp as dsp
+from vui.frontend.abstract import FrontendProcessorBase
+
+
+class FrontendProcessor(FrontendProcessorBase):
+    def __init__(self, config):
+        super(FrontendProcessor, self).__init__(config)
+
+    def process_core(self, frames):
+        spectrogram = dsp.make_spectrogram(
+            frames, seg_length=self._config.seg_length, step_count=self._config.spectrums_per_sec, keep_phase=False)
+
+        fund_freqs = dsp.get_fund_freq(frames, self._config.framerate, self._config.spectrums_per_sec,
+                                       min_freq=self._config.min_fund_freq, max_freq=self._config.max_fund_freq)
+
+        _, amps = dsp.get_harmonics(
+            fund_freqs, self._config.freq_res, self._config.harmonics_count, spectrogram=spectrogram)
+
+        return amps
